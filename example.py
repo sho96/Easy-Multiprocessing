bf9e8e21d964176c7adae23610bb83d7311ef66d
sho96 <shotaro962008@gmail.com>
2023-10-14 19:41:51 +0900

    updated README

--- example.py
+++ example.py
@@ -2,13 +2,11 @@ import mp
 
 mp.importLibraries(__file__) 
 
-
 def someHeavyFunction(x):
     a = 0
     for i in range(x):
         a += i
     return a
-
 
 reuslts = mp.process(
     someHeavyFunction, 
