b20794c41d2812777c8463efb2277a20a8110ce6
sho96 <shotaro962008@gmail.com>
2023-10-14 16:02:44 +0900

    updated README.MD

--- example.py
+++ example.py
@@ -1,24 +1,15 @@
 import mp
-import numpy as np
-import os
-import sys
 
-# Running this will import all libraries that are imported to this main script
-# This makes it unnecessary to write import statements at mp.process
 mp.importLibraries(__file__) 
 
 
-# This function requires numpy and os to run
 def someHeavyFunction(x):
-    print(np.__version__)
-    print(os.cpu_count())
     a = 0
     for i in range(x):
         a += i
     return a
 
 
-# With mp.importLibraries(__file__)
 reuslts = mp.process(
     someHeavyFunction, 
     [
@@ -27,19 +18,6 @@ reuslts = mp.process(
         (10000000, ),
         (10000000, ),
     ]
-)
-
-
-# Without mp.importLibraries(__file__)
-reuslts = mp.process(
-    someHeavyFunction, 
-    [
-        (10000000, ),
-        (10000000, ),
-        (10000000, ),
-        (10000000, ),
-    ],
-    "import numpy as np\nimport os"
 )
 
 print(reuslts)
